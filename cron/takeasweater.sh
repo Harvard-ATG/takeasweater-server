adc21894b6eff16a7f46582ec80ed3a1f76f9d3a
Arthur Barrett <abarrett@fas.harvard.edu>
2025-06-03 16:58:20 -0400

    feat: added start/end timestamps to cron script

--- cron/takeasweater.sh
+++ cron/takeasweater.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -ex
-curl -H "Authorization: Bearer ${TAKEASWEATER_CRON_SECRET}" ${TAKEASWEATER_URL}/noaa_cron.php
+echo "Starting takeasweater cron job at $(date -Iminutes)"
+curl -sS -H "Authorization: Bearer ${TAKEASWEATER_CRON_SECRET}" ${TAKEASWEATER_URL}/noaa_cron.php
+echo "Completed takeasweater cron job at $(date -Iminutes)"
